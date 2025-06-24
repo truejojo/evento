b594b1874b5eda5682160e08529f5701120d829a
truejojo <johannesveh@yahoo.de>
2025-06-24 16:22:27 +0200

    Logo with link and add nav to header with styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
 import Link from 'next/link';
 import Logo from './Logo';
 
+const routerLinks = [
+  { href: '/', label: 'Home' },
+  { href: '/events/something', label: 'Events' },
+];
+
 const Header = () => {
   return (
-    <header>
+    <header className='flex justify-between items-center px-3 sm:px-6 md:px-9 lg:px-12 py-6'>
       <Logo />
-      <Link href='/'>Home</Link>
-      <Link href='/events/something'>All Events</Link>
+      <nav>
+        <ul className='flex gap-x-4'>
+          {routerLinks.map((link) => (
+            <li
+              key={link.href}
+              className='text-white/50 hover:text-white transition-colors duration-200'
+            >
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <Image
-      src='https://bytegrad.com/course-assets/react-nextjs/evento.png'
-      alt='Evento Logo'
-      width={53}
-      height={12}
-    />
+    <Link href='/'>
+      <Image
+        src='https://bytegrad.com/course-assets/react-nextjs/evento.png'
+        alt='Evento Logo'
+        width={53}
+        height={12}
+      />
+    </Link>
   );
 };
 
