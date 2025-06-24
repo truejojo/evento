fb30e52155a42668b401f2b90ae157e04bb9f080
truejojo <johannesveh@yahoo.de>
2025-06-24 15:37:37 +0200

    Add Logo with external image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Image from 'next/image';
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Image
+      src='https://bytegrad.com/course-assets/react-nextjs/evento.png'
+      alt='Evento Logo'
+      width={53}
+      height={12}
+    />
+  );
 };
 
 export default Logo;
