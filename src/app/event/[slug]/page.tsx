05069c806e1179b93f27135ff862e386c5897a0b
truejojo <johannesveh@yahoo.de>
2025-06-27 13:25:01 +0200

    Styling Single Event Page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from '@/components/H1';
 import { URL } from '@/constants';
 import { EventProps } from '@/types';
 import Image from 'next/image';
@@ -14,9 +15,11 @@ const EventPage = async ({ params }: EventPageProps) => {
   const response = await fetch(`${URL}/events/${slug}`);
   const event: EventProps = await response.json();
 
+  const date = new Date(event.date);
+
   return (
-    <main>
-      <section className='relative h-[361px] overflow-hidden'>
+    <main className='relative'>
+      <section className='relative h-[400px] lg:h-[361px] overflow-hidden flex items-center justify-center'>
         <Image
           src={event.imageUrl}
           className='object-cover blur-3xl'
@@ -26,35 +29,38 @@ const EventPage = async ({ params }: EventPageProps) => {
           sizes='(maxwidth: 1280px) 100vw, 1280px'
           priority
         />
+        <div className='z-1 relative flex flex-col lg:flex-row gap-4 lg:gap-16 rounded-md'>
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={220}
+            className='rounded-md'
+          />
+          <div className='flex flex-col gap-2 justify-center items-center lg:items-start'>
+            <p className='text-white/75'>{date.toLocaleDateString()}</p>
+            <H1>{event.name}</H1>
+            <p className='text-white/50'>Organized by {event.organizerName}</p>
+            <button
+              className='bg-white/15 p-1 rounded-md hover:cursor-pointer hover:bg-white/25 transition-colors lg:mt-auto w-full'
+              type='button'
+            >
+              Get Tickets
+            </button>
+          </div>
+        </div>
       </section>
+      <div className='top-[400px] lg:top-[361px] left-0 right-0 text-center pt-20 pb-20 pl-20 pr-20 lg:pl-60 lg:pr-60 flex flex-col gap-20'>
+        <section>
+          <h2 className='bold text-3xl mb-3'>About this event</h2>
+          <p className='leading-8'>{event.description}</p>
+        </section>
+        <section>
+          <h2 className='bold text-3xl mb-3'>Location</h2>
+          <p className='leading-8'>{event.location}</p>
+        </section>
+      </div>
     </main>
-    // <article className='relative'>
-    //   <div
-    //     className='absolute top-0 left-0 right-0 h-[300px] bg-white/30 backdrop-blur-sm flex'
-    //     style={{ backgroundImage: `url(${event.imageUrl})` }}
-    //   >
-    //     <div className=''>
-    //       <Image
-    //         src={event.imageUrl}
-    //         alt={event.name}
-    //         width={300}
-    //         height={220}
-    //       />
-    //     </div>
-    //     <div>
-    //       <p>{event.date.toLocaleString()}</p>
-    //       <h1>{event.name}</h1>
-    //       <p>Organized by {event.organizerName}</p>
-    //       <button type='button'>Get Tickets</button>
-    //     </div>
-    //   </div>
-    //   <div className='absolute top-[300px] left-0 right-0'>
-    //     <h2>About this event</h2>
-    //     <p>{event.description}</p>
-    //     <h2>Location</h2>
-    //     <p>{event.location}</p>
-    //   </div>
-    // </article>
   );
 };
 
--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -2,7 +2,7 @@ type H1Props = {
   children: React.ReactNode;
 };
 const H1 = ({ children }: H1Props) => {
-  return <h1 className='text-6xl font-bold leading-none'>{children}</h1>;
+  return <h1 className='text-4xl font-bold leading-none'>{children}</h1>;
 };
 
 export default H1;
